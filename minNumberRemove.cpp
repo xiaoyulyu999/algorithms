ad998229b49fedeb7aacd853c3ca97c8fdb9205e
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-12-04 01:21:39 +0000

    init

---
+++ minNumberRemove.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Xiaoyu Lyu on 03/12/2025.
+//
+// Give a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping
+
+#include "minNumberRemove.h"
---
+++ minNumberRemove.h
@@ -0,0 +1,17 @@
+//
+// Created by Xiaoyu Lyu on 03/12/2025.
+//
+
+#ifndef ALGRITHMS_MINNUMBERREMOVE_H
+#define ALGRITHMS_MINNUMBERREMOVE_H
+#include <vector>
+
+
+class minNumberRemove
+{
+    public:
+    int solution(std::vector<std::pair<int , int>>*);
+};
+
+
+#endif //ALGRITHMS_MINNUMBERREMOVE_H
